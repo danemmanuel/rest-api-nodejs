09f6b8b856068c0157494af4f6ccceaf3f63ad6f
Dan Emmanuel Vieira <danemmanuelturibio@gmail.com>
2019-03-31 20:31:26 -0300

    substituição das funções para arrow function

--- api/controllers/produtoController.js
+++ api/controllers/produtoController.js
@@ -3,47 +3,47 @@
 var mongoose = require('mongoose'),
   Produto = mongoose.model('Produtos');
 
-exports.findAll = function(req, res) {
-  Produto.find({}, function(err, task) {
+exports.findAll = (req, res) => {
+  Produto.find({}, (err, task) => {
     if (err) res.send(err);
     res.json(task);
   });
 };
 
-exports.create = function(req, res) {
+exports.create = (req, res) => {
   var new_task = new Produto(req.body);
-  new_task.save(function(err, task) {
+  new_task.save((err, task) => {
     if (err) res.send(err);
     res.json(task);
   });
 };
 
-exports.findById = function(req, res) {
-  Produto.findById(req.params.taskId, function(err, task) {
+exports.findById = (req, res) => {
+  Produto.findById(req.params.taskId, (err, task) => {
     if (err) res.send(err);
     res.json(task);
   });
 };
 
-exports.updateById = function(req, res) {
+exports.updateById = (req, res) => {
   Produto.findOneAndUpdate(
     { _id: req.params.taskId },
     req.body,
     { new: true },
-    function(err, task) {
+    (err, task) => {
       if (err) res.send(err);
       res.json(task);
     }
   );
 };
 
-exports.deleteById = function(req, res) {
+exports.deleteById = (req, res) => {
   req.body.ativo = false;
   Produto.findOneAndUpdate(
     { _id: req.params.taskId },
     req.body,
     { new: true },
-    function(err, task) {
+    (err, task) => {
       if (err) res.send(err);
       res.json(task);
     }
